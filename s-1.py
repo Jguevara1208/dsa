3e53e926deb869633fb8ae5d270a88793ec8ac30
Jordan <jordansacct@gmail.com>
2021-12-02 15:24:05 -0800

    create linked list from an array of values

--- s-1.py
+++ s-1.py
@@ -270,3 +270,22 @@ def insert_node(head, value, index):
     curr_node = curr_node.next
 
   return head
+
+
+"""
+create linked list
+Write a function, create_linked_list, that takes in a list of values as an argument. The function should create a linked list containing each item of the list as the values of the nodes. The function should return the head of the linked list.
+
+test_00:
+create_linked_list(["h", "e", "y"])
+# h -> e -> y
+"""
+
+
+def create_linked_list(values):
+  dummy = Node(None)
+  tail = dummy
+  for val in values:
+    tail.next = Node(val)
+    tail = tail.next
+  return dummy.next
