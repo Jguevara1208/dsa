41fa7975921034cb17bc23f6d274cb04da51381f
Jordan <jordansacct@gmail.com>
2021-12-02 15:01:13 -0800

    finished longest_streak linked list

--- s-1.py
+++ s-1.py
@@ -138,3 +138,46 @@ def is_univalue_list(head):
       return False
     head = head.next
   return True
+
+
+"""
+longest streak
+Write a function, longest_streak, that takes in the head of a linked list as an argument. The function should return the length of the longest consecutive streak of the same value within the list.
+
+test_00:
+a = Node(5)
+b = Node(5)
+c = Node(7)
+d = Node(7)
+e = Node(7)
+f = Node(6)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+# 5 -> 5 -> 7 -> 7 -> 7 -> 6
+
+longest_streak(a) # 3
+  """
+
+def longest_streak(head):
+  max = 0
+  curr = 0
+  prev = None
+  curr_node = head
+
+  while curr_node is not None:
+      if curr_node.val == prev:
+        curr += 1
+      else:
+        curr = 1
+
+      prev = curr_node.val
+      if curr > max:
+        max = curr
+
+      curr_node = curr_node.next
+  return max
