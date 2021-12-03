7537f67aa16353d012c8753eb73670ec373a2bf1
Jordan <jordansacct@gmail.com>
2021-12-03 13:37:40 -0800

    finished linked lists

--- linked-lists.py
+++ linked-lists.py
@@ -289,3 +289,75 @@ def create_linked_list(values):
     tail.next = Node(val)
     tail = tail.next
   return dummy.next
+
+
+"""
+add lists
+Write a function, add_lists, that takes in the head of two linked lists, each representing a number. The nodes of the linked lists contain digits as values. The nodes in the input lists are reversed; this means that the least significant digit of the number is the head. The function should return the head of a new linked listed representing the sum of the input lists. The output list should have it's digits reversed as well.
+
+Say we wanted to compute 621 + 354 normally. The sum is 975:
+
+   621
+ + 354
+ -----
+   975
+
+Then, the reversed linked list format of this problem would appear as:
+
+    1 -> 2 -> 6
+ +  4 -> 5 -> 3
+ --------------
+    5 -> 7 -> 9
+test_00:
+#   621
+# + 354
+# -----
+#   975
+
+a1 = Node(1)
+a2 = Node(2)
+a3 = Node(6)
+a1.next = a2
+a2.next = a3
+# 1 -> 2 -> 6
+
+b1 = Node(4)
+b2 = Node(5)
+b3 = Node(3)
+b1.next = b2
+b2.next = b3
+# 4 -> 5 -> 3
+
+add_lists(a1, b1)
+# 5 -> 7 -> 9
+"""
+
+def add_lists(head_1, head_2):
+  dummy_head = Node(None)
+  tail = dummy_head
+  remainder = False
+
+  while head_1 is not None or head_2 is not None:
+
+    val_1 = 0 if head_1 is None else head_1.val
+    val_2 = 0 if head_2 is None else head_2.val
+    value = val_1 + val_2
+
+    if remainder is True:
+      value += 1
+
+    if value >= 10:
+      value = value - 10
+      remainder = True
+    else:
+      remainder = False
+
+    tail.next = Node(value)
+    tail = tail.next
+    head_1 = None if head_1 is None else head_1.next
+    head_2 = None if head_2 is None else head_2.next
+
+  if remainder is True:
+    tail.next = Node(1)
+
+  return dummy_head.next
